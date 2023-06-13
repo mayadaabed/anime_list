c071ed82b214daba738149b58296a3dcc8bb5280
Mayada Almobayed <abedmayada4@gmail.com>
2023-06-13 17:40:00 +0300

    create homeView route

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import '../config/dependency_injection.dart';
 import '../core/resources/manager_strings.dart';
+import '../featuers/home/presentation/view/home_view.dart';
 import '../featuers/out_boarding/presentation/view/out_boarding_view.dart';
 import '../featuers/splash/presentation/view/splash_view.dart';
 
 class Routes {
   static const String splashView = '/splash_view';
   static const String outBoardingView = '/out_boarding_view';
+  static const String homeView = '/home_view';
 }
 
 class RouteGenerator {
@@ -18,6 +20,8 @@ class RouteGenerator {
       case Routes.outBoardingView:
         initOutBoarding();
         return MaterialPageRoute(builder: (_) => const OutBoardingView());
+      case Routes.homeView:
+        return MaterialPageRoute(builder: (_) => const HomeView());
       default:
         return undefinedRoute();
     }
