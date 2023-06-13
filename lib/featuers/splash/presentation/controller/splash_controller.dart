532b09213c65306ca01d50acf260b253279ee9b5
Mayada Almobayed <abedmayada4@gmail.com>
2023-06-13 14:05:41 +0300

    refactor SplashController

--- lib/featuers/splash/presentation/controller/splash_controller.dart
+++ lib/featuers/splash/presentation/controller/splash_controller.dart
@@ -1,3 +1,17 @@
 import 'package:get/get.dart';
 
-class SplashController extends GetxController {}
+import '../../../../config/constants.dart';
+import '../../../../routes/routes.dart';
+
+class SplashController extends GetxController {
+  @override
+  void onInit() {
+    super.onInit();
+    Future.delayed(
+        const Duration(
+          seconds: Constants.splashDuration,
+        ), () {
+      Get.offAllNamed(Routes.outBoardingView);
+    });
+  }
+}
