aa0539027565423886bdc64144080761b8df3cc3
Mayada Almobayed <abedmayada4@gmail.com>
2023-06-13 17:37:57 +0300

    add strings to ManagerStrings file

--- lib/core/resources/manager_strings.dart
+++ lib/core/resources/manager_strings.dart
@@ -1,3 +1,15 @@
 class ManagerStrings {
   static const String noRoutFound = 'No Rout Found';
+  static const String animeXD = 'AnimeXD';
+  static const String getStartButton = 'Get started';
+  static const String skip = 'skip';
+  static const String outBoardingTitle1 = 'Unlimited Anime';
+  static const String outBoardingTitle2 = 'HD Quality Streaming';
+  static const String outBoardingTitle3 = 'Downloads';
+  static const String outBoardingSubTitle1 =
+      'Watch any Anime of your choice from a wide array of available collections.';
+  static const String outBoardingSubTitle2 =
+      'Experinced and Enjoy watching your favorite Animes in HD.';
+  static const String outBoardingSubTitle3 =
+      'Download your favorite Animes to watch them later.';
 }
