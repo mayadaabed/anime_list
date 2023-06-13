2b536f8e23c0f25c7670c844bf262c8570a185f8
Mayada Almobayed <abedmayada4@gmail.com>
2023-06-13 17:35:36 +0300

    add outBoardingDurationTime & sliderItems

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -2,6 +2,8 @@ class Constants {
   static const double deviceWidth = 375.0;
   static const double deviceHeigth = 812.0;
   static const int splashDuration = 3;
+  static const int outBoardingDurationTime = 1;
+  static const int sliderItems = 3;
 }
 
 class ConstantsPrefsKeys {
