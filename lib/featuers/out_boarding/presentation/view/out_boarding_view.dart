ca81be680d6e5d87604643c1ac4b85f2231d7f7d
Mayada Almobayed <abedmayada4@gmail.com>
2023-06-13 23:50:22 +0300

    refactor

--- lib/featuers/out_boarding/presentation/view/out_boarding_view.dart
+++ lib/featuers/out_boarding/presentation/view/out_boarding_view.dart
@@ -7,6 +7,7 @@ import '../../../../core/resources/manager_sizes.dart';
 import '../../../../core/resources/manager_strings.dart';
 import '../../../../core/resources/manager_styles.dart';
 import '../../../../core/widgets/main_button.dart';
+import '../../../../core/widgets/will_pop_scope.dart';
 import '../controller/out_boarding_controller.dart';
 import 'widget/slider_indicator.dart';
 
@@ -15,74 +16,76 @@ class OutBoardingView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: ManagerColors.backgroundColor,
-      body: SafeArea(
-        child: Container(
-          margin: EdgeInsets.symmetric(
-            horizontal: ManagerWidth.w16,
-            vertical: ManagerHeight.h10,
-          ),
-          child: GetBuilder<OutBoardingController>(builder: (controller) {
-            return Column(
-              children: [
-                Visibility(
-                  visible: controller.isNotLastedPage(),
-                  maintainSize: true,
-                  maintainState: true,
-                  maintainAnimation: true,
-                  child: Align(
-                      alignment: AlignmentDirectional.centerEnd,
-                      child: mainButton(
-                        onPressed: () {
-                          controller.skipPage();
-                        },
-                        child: Text(
-                          ManagerStrings.skip,
-                          style: getRegularTextStyle(
-                              fontSize: ManagerFontSize.s14,
-                              color: ManagerColors.textColor),
+    return willPopScope(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: ManagerColors.backgroundColor,
+        body: SafeArea(
+          child: Container(
+            margin: EdgeInsets.symmetric(
+              horizontal: ManagerWidth.w16,
+              vertical: ManagerHeight.h10,
+            ),
+            child: GetBuilder<OutBoardingController>(builder: (controller) {
+              return Column(
+                children: [
+                  Visibility(
+                    visible: controller.isNotLastedPage(),
+                    maintainSize: true,
+                    maintainState: true,
+                    maintainAnimation: true,
+                    child: Align(
+                        alignment: AlignmentDirectional.centerEnd,
+                        child: mainButton(
+                          onPressed: () {
+                            controller.skipPage();
+                          },
+                          child: Text(
+                            ManagerStrings.skip,
+                            style: getRegularTextStyle(
+                                fontSize: ManagerFontSize.s14,
+                                color: ManagerColors.textColor),
+                          ),
+                        )),
+                  ),
+                  SizedBox(
+                    height: ManagerHeight.h20,
+                  ),
+                  Expanded(
+                    child: PageView(
+                      controller: controller.pageController,
+                      children: [...controller.pageViewItems],
+                      onPageChanged: (index) {
+                        controller.setCurrentPage(index);
+                      },
+                    ),
+                  ),
+                  SizedBox(height: ManagerHeight.h20),
+                  mainButton(
+                      onPressed: () {
+                        controller.getStart();
+                      },
+                      color: ManagerColors.primaryColor,
+                      minWidth: double.infinity,
+                      height: ManagerHeight.h54,
+                      child: Text(
+                        ManagerStrings.getStartButton,
+                        style: getBoldTextStyle(
+                          fontSize: ManagerFontSize.s18,
+                          color: ManagerColors.black,
                         ),
                       )),
-                ),
-                SizedBox(
-                  height: ManagerHeight.h20,
-                ),
-                Expanded(
-                  child: PageView(
-                    controller: controller.pageController,
-                    children: [...controller.pageViewItems],
-                    onPageChanged: (index) {
-                      controller.setCurrentPage(index);
-                    },
+                  SizedBox(
+                    height: ManagerHeight.h20,
                   ),
-                ),
-                SizedBox(height: ManagerHeight.h20),
-                mainButton(
-                    onPressed: () {
-                      controller.getStart();
-                    },
-                    color: ManagerColors.primaryColor,
-                    minWidth: double.infinity,
-                    height: ManagerHeight.h54,
-                    child: Text(
-                      ManagerStrings.getStartButton,
-                      style: getBoldTextStyle(
-                        fontSize: ManagerFontSize.s18,
-                        color: ManagerColors.black,
-                      ),
-                    )),
-                SizedBox(
-                  height: ManagerHeight.h20,
-                ),
-                const SliderIndicator(),
-                SizedBox(
-                  height: ManagerHeight.h20,
-                ),
-              ],
-            );
-          }),
+                  const SliderIndicator(),
+                  SizedBox(
+                    height: ManagerHeight.h20,
+                  ),
+                ],
+              );
+            }),
+          ),
         ),
       ),
     );
--- lib/featuers/splash/presentation/view/splash_view.dart
+++ lib/featuers/splash/presentation/view/splash_view.dart
@@ -4,20 +4,23 @@ import 'package:anime_list/core/resources/manager_strings.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../core/resources/manager_styles.dart';
+import '../../../../core/widgets/will_pop_scope.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: ManagerColors.backgroundColor,
-      body: Center(
-          child: Text(ManagerStrings.animeXD,
-              style: getBoldTextStyle(
-                fontSize: ManagerFontSize.s40,
-                color: ManagerColors.primaryColor,
-              ))),
+    return willPopScope(
+      child: Scaffold(
+        backgroundColor: ManagerColors.backgroundColor,
+        body: Center(
+            child: Text(ManagerStrings.animeXD,
+                style: getBoldTextStyle(
+                  fontSize: ManagerFontSize.s40,
+                  color: ManagerColors.primaryColor,
+                ))),
+      ),
     );
   }
 }
