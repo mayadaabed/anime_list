c3fa03cab7535377e47b2ccf9303a71b4dc5e84f
Mayada Almobayed <abedmayada4@gmail.com>
2023-06-13 12:27:21 +0300

    create constants file

---
+++ lib/config/constants.dart
@@ -0,0 +1,9 @@
+class Constants {
+  static const double deviceWidth = 375.0;
+  static const double deviceHeigth = 812.0;
+  static const int splashDuration = 3;
+}
+
+class ConstantsPrefsKeys {
+  static const String outBoardingViewedKey = 'out_boarding_viewed';
+}
