184340d393e0b95e8628ea74adb349a03a47a407
Mayada Almobayed <abedmayada4@gmail.com>
2023-06-13 23:51:05 +0300

    refactor HomeView

--- lib/featuers/home/presentation/view/home_view.dart
+++ lib/featuers/home/presentation/view/home_view.dart
@@ -1,10 +1,20 @@
+import 'package:anime_list/core/resources/manager_colors.dart';
 import 'package:flutter/material.dart';
+
+import '../../../../core/widgets/will_pop_scope.dart';
+import 'widget/home_app_bar.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return willPopScope(
+      child: Scaffold(
+        backgroundColor: ManagerColors.backgroundColor,
+        appBar: homeAppBar(),
+        // body: ,
+      ),
+    );
   }
 }
