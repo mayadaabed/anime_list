22a8fc70a8f76a6053548f04159f97b96c01ad70
Mayada Almobayed <abedmayada4@gmail.com>
2023-06-14 00:47:20 +0300

    refactor HomeController

--- lib/featuers/home/presentation/controller/home_controller.dart
+++ lib/featuers/home/presentation/controller/home_controller.dart
@@ -1,3 +1,20 @@
+import 'package:carousel_slider/carousel_controller.dart';
 import 'package:get/get.dart';
 
-class HomeController extends GetxController {}
+class HomeController extends GetxController {
+  late CarouselController carouselController;
+  int current = 0;
+
+  List sliders = [];
+
+  @override
+  void onInit() {
+    super.onInit();
+    carouselController = CarouselController();
+  }
+
+  void change(int index) {
+    current = index;
+    update();
+  }
+}
