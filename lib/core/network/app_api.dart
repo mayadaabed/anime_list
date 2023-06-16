24356b16f17e75895e15436b135b579975f438b9
Mayada Almobayed <abedmayada4@gmail.com>
2023-06-16 23:32:00 +0300

    add trendingAnime & manga Reqs

--- lib/core/network/app_api.dart
+++ lib/core/network/app_api.dart
@@ -1,6 +1,9 @@
 import 'package:dio/dio.dart';
 import 'package:retrofit/http.dart';
 import '../../config/constants.dart';
+import '../../config/request_constants.dart';
+import '../../featuers/home/data/response/manga_response/manga_response.dart';
+import '../../featuers/home/data/response/trending_anime_response.dart';
 
 part 'app_api.g.dart';
 
@@ -11,5 +14,9 @@ abstract class AppApi {
     String baseUrl,
   }) = _AppApi;
 
+  @GET(RequestConstants.trendingAnime)
+  Future<TrendingAnimeResponse> trendingAnime();
 
+  @GET(RequestConstants.manga)
+  Future<MangaResponse> manga();
 }
