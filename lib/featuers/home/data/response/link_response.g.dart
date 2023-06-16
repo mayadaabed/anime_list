65e787d002d6fd6a0ec935f081a16c2fae3175af
Mayada Almobayed <abedmayada4@gmail.com>
2023-06-16 23:42:45 +0300

    create LinkResponse

---
+++ lib/featuers/home/data/response/link_response.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'link_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+LinkResponse _$LinkResponseFromJson(Map<String, dynamic> json) =>
+    LinkResponse()..self = json['self'] as String?;
+
+Map<String, dynamic> _$LinkResponseToJson(LinkResponse instance) =>
+    <String, dynamic>{
+      'self': instance.self,
+    };
