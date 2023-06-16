b91323768aa3221e62a9a00c1d2a60943818bc3c
Mayada Almobayed <abedmayada4@gmail.com>
2023-06-16 23:32:49 +0300

    create DioFactory in core folder

---
+++ lib/core/network/dio_factory.dart
@@ -0,0 +1,35 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+import '../../config/constants.dart';
+
+class DioFactory {
+  Future<Dio> getDio() async {
+    Dio dio = Dio();
+    dio.options = BaseOptions(
+      baseUrl: ApiConstants.baseUrl,
+      receiveTimeout:
+          const Duration(seconds: ApiConstants.recieveTimeOutDuration),
+      sendTimeout: const Duration(seconds: ApiConstants.sendTimeOutDuration),
+    );
+
+    InterceptorsWrapper interceptorsWrapper = InterceptorsWrapper(onRequest:
+        (RequestOptions options, RequestInterceptorHandler handler) async {
+      return handler.next(options);
+    });
+
+    dio.interceptors.add(interceptorsWrapper);
+
+    if (!kReleaseMode) {
+      dio.interceptors.add(PrettyDioLogger(
+        responseBody: true,
+        requestBody: true,
+        responseHeader: true,
+        requestHeader: true,
+        request: true,
+      ));
+    }
+
+    return dio;
+  }
+}
