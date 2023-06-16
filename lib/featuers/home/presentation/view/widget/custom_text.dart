b51beb058abb1bc1c5974afeed2d1fc317c4f959
Mayada Almobayed <abedmayada4@gmail.com>
2023-06-16 23:53:05 +0300

    create CustomText

---
+++ lib/featuers/home/presentation/view/widget/custom_text.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/resources/manager_colors.dart';
+import '../../../../../core/resources/manager_fonts.dart';
+import '../../../../../core/resources/manager_sizes.dart';
+import '../../../../../core/resources/manager_styles.dart';
+
+class CustomText extends StatelessWidget {
+  const CustomText(
+      {Key? key,
+      required this.name,
+      required this.nameButton,
+      required this.buttonColor})
+      : super(key: key);
+
+  final String name;
+  final String nameButton;
+  final Color buttonColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+          horizontal: ManagerWidth.w10, vertical: ManagerHeight.h20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            name,
+            style: getBoldTextStyle(
+              fontSize: ManagerFontSize.s16,
+              color: ManagerColors.textColor,
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              nameButton,
+              style: getMediumTextStyle(
+                fontSize: ManagerFontSize.s14,
+                color: buttonColor,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
