ae6803cb70a242597fa9842e5c19629e8fa3c1ff
Mayada Almobayed <abedmayada4@gmail.com>
2023-06-16 23:54:26 +0300

    edit CustomBanner

--- lib/featuers/home/presentation/view/widget/custom_banner.dart
+++ lib/featuers/home/presentation/view/widget/custom_banner.dart
@@ -22,60 +22,83 @@ class CustomBanner extends StatelessWidget {
             scrollPhysics: const BouncingScrollPhysics(),
             viewportFraction: 0.85,
             autoPlay: true,
-            height: ManagerHeight.h160,
+            height: ManagerHeight.h250,
             onPageChanged: (index, reason) {
               controller.change(index);
             },
           ),
-          items: controller.sliders.map((i) {
-            return Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: ManagerWidth.w10,
-                vertical: ManagerHeight.h10,
-              ),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(ManagerRadius.r14),
-                image: DecorationImage(
-                    image: i.attributeModel!.image!.isNotEmpty
-                        ? NetworkImage(i.attributeModel!.image!.toString())
-                        : const AssetImage(ManagerAssets.outBoardingImag3)
-                            as ImageProvider,
-                    fit: BoxFit.cover),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    i.attributeModel!.title.toString(),
-                    style: getRegularTextStyle(
-                      fontSize: ManagerFontSize.s16,
-                      color: ManagerColors.white,
+          items: controller.trendingAnimeList.map((i) {
+            return Stack(
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: ManagerWidth.w10,
+                    vertical: ManagerHeight.h10,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(ManagerRadius.r14),
+                    image: DecorationImage(
+                      image: i.attributesResponse!.posterImage!.large
+                              .toString()
+                              .isNotEmpty
+                          ? NetworkImage(i
+                              .attributesResponse!.posterImage!.large
+                              .toString())
+                          : const AssetImage(ManagerAssets.outBoardingImag3)
+                              as ImageProvider,
+                      fit: BoxFit.cover,
                     ),
                   ),
-                  Row(
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: ManagerWidth.w10,
+                    vertical: ManagerHeight.h10,
+                  ),
+                  decoration: BoxDecoration(
+                    color: ManagerColors.black.withOpacity(
+                      ManagerOpacity.op0_2,
+                    ),
+                    borderRadius: BorderRadius.circular(ManagerRadius.r14),
+                  ),
+                  child: Column(
                     mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      ...List.generate(
-                        controller.sliders.length,
-                        (index) => Container(
-                          width: ManagerWidth.w8,
-                          height: ManagerHeight.h8,
-                          margin: EdgeInsets.only(left: ManagerWidth.w2),
-                          decoration: BoxDecoration(
-                            color: controller.current == index
-                                ? ManagerColors.primaryColor
-                                : ManagerColors.greyLight,
-                            borderRadius: BorderRadius.circular(
-                              ManagerRadius.r10,
+                      Text(
+                        i.attributesResponse!.titles!.en.toString(),
+                        style: getBoldTextStyle(
+                          fontSize: ManagerFontSize.s16,
+                          color: ManagerColors.white,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          ...List.generate(
+                            controller.trendingAnimeList.length,
+                            (index) => Container(
+                              width: ManagerWidth.w8,
+                              height: ManagerHeight.h8,
+                              margin: EdgeInsets.only(left: ManagerWidth.w2),
+                              decoration: BoxDecoration(
+                                color: controller.current == index
+                                    ? ManagerColors.primaryColor
+                                    : ManagerColors.primaryColor.withOpacity(
+                                        ManagerOpacity.op0_5,
+                                      ),
+                                borderRadius: BorderRadius.circular(
+                                  ManagerRadius.r10,
+                                ),
+                              ),
                             ),
                           ),
-                        ),
+                        ],
                       ),
                     ],
                   ),
-                ],
-              ),
+                ),
+              ],
             );
           }).toList(),
         );
