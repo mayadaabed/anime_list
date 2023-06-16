86db63e4f38441af6cd0e71080b9648931935c56
Mayada Almobayed <abedmayada4@gmail.com>
2023-06-16 23:54:07 +0300

    edit HomeView

--- lib/featuers/home/presentation/view/home_view.dart
+++ lib/featuers/home/presentation/view/home_view.dart
@@ -1,8 +1,14 @@
 import 'package:anime_list/core/resources/manager_colors.dart';
+import 'package:anime_list/core/resources/manager_sizes.dart';
+import 'package:anime_list/core/resources/manager_strings.dart';
 import 'package:anime_list/featuers/home/presentation/view/widget/custom_banner.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../../../../core/widgets/will_pop_scope.dart';
+import '../controller/home_controller.dart';
+import 'widget/custom_top_anime.dart';
+import 'widget/custom_text.dart';
 import 'widget/home_app_bar.dart';
 
 class HomeView extends StatelessWidget {
@@ -14,11 +20,48 @@ class HomeView extends StatelessWidget {
       child: Scaffold(
         backgroundColor: ManagerColors.backgroundColor,
         appBar: homeAppBar(),
-        body: ListView(
-          children: [
-            CustomBanner(),
-          ],
-        ),
+        body: GetBuilder<HomeController>(builder: (controller) {
+          return ListView(
+            children: [
+              SizedBox(
+                height: ManagerHeight.h30,
+              ),
+              const CustomBanner(),
+              const CustomText(
+                name: ManagerStrings.topAnime,
+                buttonColor: ManagerColors.primaryColor,
+                nameButton: ManagerStrings.seeAll,
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: ManagerHeight.h250,
+                child: ListView.builder(
+                  itemCount: controller.trendingAnimeList.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return CustomTopAnime(
+                      name: controller.trendingAnimeList[index]
+                          .attributesResponse!.titles!.en
+                          .toString(),
+                      imagePath: controller.trendingAnimeList[index]
+                          .attributesResponse!.posterImage!.large
+                          .toString(),
+                      index: index,
+                      episode: controller.trendingAnimeList[index]
+                          .attributesResponse!.episodeCount!
+                          .toString(),
+                    );
+                  },
+                ),
+              ),
+              const CustomText(
+                name: ManagerStrings.manga,
+                buttonColor: ManagerColors.primaryColor,
+                nameButton: ManagerStrings.seeAll,
+              ),
+            ],
+          );
+        }),
       ),
     );
   }
