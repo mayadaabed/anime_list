cd54c0da39467396d59e05aba9567533e0b97693
Mayada Almobayed <abedmayada4@gmail.com>
2023-06-16 23:48:26 +0300

    create DimensionsModel

---
+++ lib/featuers/home/domain/model/dimensions_model.dart
@@ -0,0 +1,15 @@
+import 'dimensions_details_model.dart';
+
+class DimensionsModel {
+  DimensionsDetailsModel? tiny;
+  DimensionsDetailsModel? large;
+  DimensionsDetailsModel? small;
+  DimensionsDetailsModel? medium;
+
+  DimensionsModel({
+    this.tiny,
+    this.large,
+    this.small,
+    this.medium,
+  });
+}
