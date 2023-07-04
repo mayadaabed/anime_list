6c96807e16e79f365315f7636de72f014654d119
Mayada Almobayed <abedmayada4@gmail.com>
2023-07-04 14:55:02 +0300

    create SearchImagesModel

---
+++ lib/featuers/search/domain/model/search_images_model.dart
@@ -0,0 +1,15 @@
+class SearchImagesModel {
+  String? imageUrl;
+  String? smallImageUrl;
+  String? mediumImageUrl;
+  String? largeImageUrl;
+  String? maximumImageUrl;
+
+  SearchImagesModel({
+    this.imageUrl,
+    this.smallImageUrl,
+    this.mediumImageUrl,
+    this.largeImageUrl,
+    this.maximumImageUrl,
+  });
+}
