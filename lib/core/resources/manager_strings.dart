8abd99f527bdeabbadc5a9bac87b504e76f0c55d
Mayada Almobayed <abedmayada4@gmail.com>
2023-07-24 16:22:32 +0300

    edit ManagerStrings

--- lib/core/resources/manager_strings.dart
+++ lib/core/resources/manager_strings.dart
@@ -1,24 +1,79 @@
+import 'package:easy_localization/easy_localization.dart';
+
 class ManagerStrings {
-  static const String noRoutFound = 'No Rout Found';
-  static const String animeXD = 'AnimeXD';
-  static const String getStartButton = 'Get started';
-  static const String skip = 'skip';
-  static const String outBoardingTitle1 = 'Unlimited Anime';
-  static const String outBoardingTitle2 = 'HD Quality Streaming';
-  static const String outBoardingTitle3 = 'Downloads';
-  static const String outBoardingSubTitle1 =
-      'Watch any Anime of your choice from a wide array of available collections.';
-  static const String outBoardingSubTitle2 =
-      'Experinced and Enjoy watching your favorite Animes in HD.';
-  static const String outBoardingSubTitle3 =
-      'Download your favorite Animes to watch them later.';
-  static const String ok = 'Ok';
-  static const String hello = 'Hello';
-  static const String otukao = 'Outokau';
-  static const String topAnime = 'Top Anime';
-  static const String seeAll = 'see All';
-  static const String episode = 'Episode';
-  static const String manga = 'Manga';
-  static const String chapters = 'Chapters';
-  static const String search = 'Search';
+  static String get noRoutFound => tr('noRoutFound');
+
+  static String get animeXD => tr('animeXD');
+
+  static String get getStartButton => tr('getStartButton');
+
+  static String get skip => tr('skip');
+
+  static String get outBoardingTitle1 => tr('outBoardingTitle1');
+
+  static String get outBoardingTitle2 => tr('outBoardingTitle2');
+
+  static String get outBoardingTitle3 => tr('outBoardingTitle3');
+
+  static String get outBoardingSubTitle1 => tr('outBoardingSubTitle1');
+
+  static String get outBoardingSubTitle2 => tr('outBoardingSubTitle2');
+
+  static String get outBoardingSubTitle3 => tr('outBoardingSubTitle3');
+
+  static String get ok => tr('ok');
+
+  static String get hello => tr('hello');
+
+  static String get otukao => tr('otukao');
+
+  static String get topAnime => tr('topAnime');
+
+  static String get seeAll => tr('seeAll');
+
+  static String get episode => tr('episode');
+
+  static String get manga => tr('manga');
+
+  static String get chapters => tr('chapters');
+
+  static String get search => tr('search');
+
+  static String get schedules => tr('schedules');
+
+  static String get profile => tr('profile');
+
+  static String get season => tr('season');
+
+  static String get date => tr('date');
+
+  static String get allAnime => tr('allAnime');
+
+  static String get allManga => tr('allManga');
+
+  static String get animeDetails => tr('animeDetails');
+
+  static String get released => tr('released');
+
+  static String get rating => tr('rating');
+
+  static String get trailer => tr('trailer');
+
+  static String get story => tr('story');
+
+  static String get changeLanguage => tr('changeLanguage');
+
+  static String get appTheme => tr('appTheme');
+
+  static String get logout => tr('logout');
+
+  static String get darkMode => tr('darkMode');
+
+  static String get on => tr('on');
+
+  static String get off => tr('off');
+
+  static String get language => tr('language');
+  static String get sureLogout => tr('sureLogout');
+  static String get cancel => tr('cancel');
 }
