58606eff3e8e76f81e8df8cd132e2444d6fe0b85
Mayada Almobayed <abedmayada4@gmail.com>
2023-07-24 16:08:46 +0300

    edit CustomBanner

--- lib/featuers/home/presentation/view/widget/custom_banner.dart
+++ lib/featuers/home/presentation/view/widget/custom_banner.dart
@@ -1,12 +1,15 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+import '../../../../../core/cache/cache.dart';
 import '../../../../../core/resources/manager_assets.dart';
 import '../../../../../core/resources/manager_colors.dart';
 import '../../../../../core/resources/manager_fonts.dart';
 import '../../../../../core/resources/manager_sizes.dart';
 import '../../../../../core/resources/manager_styles.dart';
 import '../../controller/home_controller.dart';
+import '../trending_anime_details_view.dart';
 
 class CustomBanner extends StatelessWidget {
   const CustomBanner({Key? key}) : super(key: key);
@@ -28,77 +31,121 @@ class CustomBanner extends StatelessWidget {
             },
           ),
           items: controller.trendingAnimeList.map((i) {
-            return Stack(
-              children: [
-                Container(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: ManagerWidth.w10,
-                    vertical: ManagerHeight.h10,
+            return InkWell(
+              onTap: () {
+                CacheData().setYoutybeViedoId(
+                    i.attributesResponse!.youtubeVideoId!.toString());
+                controller.youtubePlayerController = YoutubePlayerController(
+                  initialVideoId: i.attributesResponse!.youtubeVideoId!,
+                  flags: const YoutubePlayerFlags(
+                    mute: false,
+                    autoPlay: false,
+                    disableDragSeek: false,
+                    loop: false,
+                    isLive: false,
+                    forceHD: false,
+                    enableCaption: true,
                   ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(ManagerRadius.r14),
-                    image: DecorationImage(
-                      image: i.attributesResponse!.posterImage!.large
-                              .toString()
-                              .isNotEmpty
-                          ? NetworkImage(i
-                              .attributesResponse!.posterImage!.large
-                              .toString())
-                          : const AssetImage(ManagerAssets.outBoardingImag3)
-                              as ImageProvider,
-                      fit: BoxFit.cover,
+                );
+                Get.to(() => TrendingAnimeDetailsView(
+                      title: i.attributesResponse!.titles!.en.toString(),
+                      coverImage:
+                          i.attributesResponse!.coverImage!.large.toString(),
+                      description:
+                          i.attributesResponse!.description!.toString(),
+                      averageRating:
+                          i.attributesResponse!.averageRating!.toString(),
+                      usreCount: i.attributesResponse!.userCount!,
+                      favoritesCount: i.attributesResponse!.favoritesCount!,
+                      startDate: i.attributesResponse!.startDate!,
+                      endDate: i.attributesResponse!.endDate!,
+                      nextRelease: i.attributesResponse!.nextRelease!,
+                      popularityRank: i.attributesResponse!.popularityRank!,
+                      ratingRank: i.attributesResponse!.ratingRank!,
+                      ageRatingGuide: i.attributesResponse!.ageRatingGuide!,
+                      posterImage: i.attributesResponse!.posterImage!.large!,
+                      episodeCount:
+                          i.attributesResponse!.episodeCount!.toString(),
+                      episodeLength:
+                          i.attributesResponse!.episodeLength!.toString(),
+                      totalLength:
+                          i.attributesResponse!.totalLength!.toString(),
+                      youtubeVideoId: i.attributesResponse!.youtubeVideoId!,
+                      showType: i.attributesResponse!.showType!,
+                    ));
+              },
+              child: Stack(
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: ManagerWidth.w10,
+                      vertical: ManagerHeight.h10,
                     ),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: ManagerWidth.w10,
-                    vertical: ManagerHeight.h10,
-                  ),
-                  decoration: BoxDecoration(
-                    color: ManagerColors.black.withOpacity(
-                      ManagerOpacity.op0_2,
-                    ),
-                    borderRadius: BorderRadius.circular(ManagerRadius.r14),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        i.attributesResponse!.titles!.en.toString(),
-                        style: getBoldTextStyle(
-                          fontSize: ManagerFontSize.s16,
-                          color: ManagerColors.white,
-                        ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(ManagerRadius.r14),
+                      image: DecorationImage(
+                        image: i.attributesResponse!.posterImage!.large
+                                .toString()
+                                .isNotEmpty
+                            ? NetworkImage(i
+                                .attributesResponse!.posterImage!.large
+                                .toString())
+                            : const AssetImage(ManagerAssets.outBoardingImag3)
+                                as ImageProvider,
+                        fit: BoxFit.cover,
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          ...List.generate(
-                            controller.trendingAnimeList.length,
-                            (index) => Container(
-                              width: ManagerWidth.w8,
-                              height: ManagerHeight.h8,
-                              margin: EdgeInsets.only(left: ManagerWidth.w2),
-                              decoration: BoxDecoration(
-                                color: controller.current == index
-                                    ? ManagerColors.primaryColor
-                                    : ManagerColors.primaryColor.withOpacity(
-                                        ManagerOpacity.op0_5,
-                                      ),
-                                borderRadius: BorderRadius.circular(
-                                  ManagerRadius.r10,
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: ManagerWidth.w10,
+                      vertical: ManagerHeight.h10,
+                    ),
+                    decoration: BoxDecoration(
+                      color: ManagerColors.black.withOpacity(
+                        ManagerOpacity.op0_2,
+                      ),
+                      borderRadius: BorderRadius.circular(ManagerRadius.r14),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          i.attributesResponse!.canonicalTitle.toString(),
+                          style: getBoldTextStyle(
+                            fontSize: ManagerFontSize.s16,
+                            color: ManagerColors.white,
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            ...List.generate(
+                              controller.trendingAnimeList.length,
+                              (index) => Container(
+                                width: ManagerWidth.w8,
+                                height: ManagerHeight.h8,
+                                margin: EdgeInsets.only(left: ManagerWidth.w2),
+                                decoration: BoxDecoration(
+                                  color: controller.current == index
+                                      ? ManagerColors.primaryColor
+                                      : ManagerColors.primaryColor.withOpacity(
+                                          ManagerOpacity.op0_5,
+                                        ),
+                                  borderRadius: BorderRadius.circular(
+                                    ManagerRadius.r10,
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                        ],
-                      ),
-                    ],
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             );
           }).toList(),
         );
