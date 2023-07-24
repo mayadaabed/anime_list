4da64d7cc697b8891ad26f3a90f9fd97584dddad
Mayada Almobayed <abedmayada4@gmail.com>
2023-07-24 16:13:56 +0300

    add requests

--- lib/core/network/app_api.dart
+++ lib/core/network/app_api.dart
@@ -2,9 +2,12 @@ import 'package:dio/dio.dart';
 import 'package:retrofit/http.dart';
 import '../../config/constants.dart';
 import '../../config/request_constants.dart';
+import '../../featuers/anime_details/data/response/anime_details_response.dart';
 import '../../featuers/home/data/response/manga_response/manga_response.dart';
 import '../../featuers/home/data/response/trending_anime_response.dart';
+import '../../featuers/schedules/data/response/schedules_response.dart';
 import '../../featuers/search/data/response/search_response.dart';
+import '../../featuers/seasons/data/response/seasons_response.dart';
 
 part 'app_api.g.dart';
 
@@ -26,4 +29,14 @@ abstract class AppApi {
     @Query('q') String? query,
   );
 
+  @GET(RequestConstants.animeDetails)
+  Future<AnimeDetailsResponse> animeDetails(
+    @Path('id') int id,
+  );
+
+  @GET(RequestConstants.schedules)
+  Future<SchedulesResponse> schedules();
+
+  @GET(RequestConstants.seasons)
+  Future<SeasonsResponse> seasons();
 }
--- lib/core/network/app_api.g.dart
+++ lib/core/network/app_api.g.dart
@@ -90,6 +90,75 @@ class _AppApi implements AppApi {
     return value;
   }
 
+  @override
+  Future<AnimeDetailsResponse> animeDetails(int id) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final Map<String, dynamic>? _data = null;
+    final _result = await _dio.fetch<Map<String, dynamic>>(
+        _setStreamType<AnimeDetailsResponse>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              'https://api.jikan.moe/v4/anime/${id}/full',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = AnimeDetailsResponse.fromJson(_result.data!);
+    return value;
+  }
+
+  @override
+  Future<SchedulesResponse> schedules() async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final Map<String, dynamic>? _data = null;
+    final _result = await _dio
+        .fetch<Map<String, dynamic>>(_setStreamType<SchedulesResponse>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              'https://api.jikan.moe/v4/schedules',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = SchedulesResponse.fromJson(_result.data!);
+    return value;
+  }
+
+  @override
+  Future<SeasonsResponse> seasons() async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final Map<String, dynamic>? _data = null;
+    final _result = await _dio
+        .fetch<Map<String, dynamic>>(_setStreamType<SeasonsResponse>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              'https://api.jikan.moe/v4/seasons/now',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = SeasonsResponse.fromJson(_result.data!);
+    return value;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
