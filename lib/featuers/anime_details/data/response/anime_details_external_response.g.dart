12a5f3cae2f29430b4fe395144a8618de8206d96
Mayada Almobayed <abedmayada4@gmail.com>
2023-07-24 15:11:00 +0300

    create AnimeDetailsExternalResponse.g

---
+++ lib/featuers/anime_details/data/response/anime_details_external_response.g.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'anime_details_external_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+AnimeDetailsExternalResponse _$AnimeDetailsExternalResponseFromJson(
+        Map<String, dynamic> json) =>
+    AnimeDetailsExternalResponse()
+      ..name = json['name'] as String?
+      ..url = json['url'] as String?;
+
+Map<String, dynamic> _$AnimeDetailsExternalResponseToJson(
+        AnimeDetailsExternalResponse instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'url': instance.url,
+    };
