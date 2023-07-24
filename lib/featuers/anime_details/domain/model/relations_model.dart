d5b91b2cddb4f8ff7825ca0b053d30b88e992fe8
Mayada Almobayed <abedmayada4@gmail.com>
2023-07-24 15:15:33 +0300

    create RelationsModel

---
+++ lib/featuers/anime_details/domain/model/relations_model.dart
@@ -0,0 +1,11 @@
+import '../../../home/domain/model/manga_models/authors_model.dart';
+
+class RelationsModel {
+  String? relation;
+  List<AuthorsModel>? entry;
+
+  RelationsModel({
+    this.relation,
+    this.entry,
+  });
+}
