aa78e508db828215589747a73c1fefddc8ddcf1b
Mayada Almobayed <abedmayada4@gmail.com>
2023-07-24 16:03:51 +0300

    create CustomProfile

---
+++ lib/featuers/profile/persentation/view/widget/custom_profile.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../../../core/resources/manager_colors.dart';
+import '../../../../../core/resources/manager_fonts.dart';
+import '../../../../../core/resources/manager_sizes.dart';
+import '../../../../../core/resources/manager_styles.dart';
+
+class CustomProfile extends StatelessWidget {
+  final String imagePath;
+  final String textName;
+  final void Function()? onTap;
+
+  const CustomProfile(
+      {Key? key, required this.imagePath, required this.textName, this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Row(
+        children: [
+          Container(
+            width: ManagerWidth.w40,
+            height: ManagerHeight.h40,
+            margin: EdgeInsets.symmetric(
+                horizontal: ManagerWidth.w14, vertical: ManagerHeight.h16),
+            padding: EdgeInsets.symmetric(
+                horizontal: ManagerWidth.w4, vertical: ManagerHeight.h4),
+            child: SvgPicture.asset(
+              imagePath,
+              colorFilter: const ColorFilter.mode(
+                ManagerColors.white,
+                BlendMode.srcIn,
+              ),
+            ),
+          ),
+          Text(
+            textName,
+            style: getRegularTextStyle(
+                fontSize: ManagerFontSize.s16, color: ManagerColors.textColor),
+          ),
+          const Spacer(),
+          Padding(
+            padding: EdgeInsets.only(right: ManagerWidth.w10),
+            child: const Icon(
+              Icons.arrow_forward_ios_rounded,
+              color: ManagerColors.white,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
