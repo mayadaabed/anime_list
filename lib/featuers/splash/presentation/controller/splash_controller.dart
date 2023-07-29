a123600908e9dd3532ebe539303d63f1f6317a2f
Mayada Almobayed <abedmayada4@gmail.com>
2023-07-29 14:41:19 +0300

    check theme

--- lib/featuers/splash/presentation/controller/splash_controller.dart
+++ lib/featuers/splash/presentation/controller/splash_controller.dart
@@ -1,12 +1,14 @@
 import 'package:get/get.dart';
 
 import '../../../../config/constants.dart';
+import '../../../../core/service/theme_service.dart';
 import '../../../../routes/routes.dart';
 
 class SplashController extends GetxController {
   @override
   void onInit() {
     super.onInit();
+    ThemeService().getThemeData();
     Future.delayed(
         const Duration(
           seconds: Constants.splashDuration,
