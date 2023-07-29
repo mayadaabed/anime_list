c07c1d4ccde072a4e491cb134398ecfbf4a4d666
Mayada Almobayed <abedmayada4@gmail.com>
2023-07-29 14:33:24 +0300

    change text color light

--- lib/core/resources/manager_colors.dart
+++ lib/core/resources/manager_colors.dart
@@ -4,7 +4,7 @@ class ManagerColors {
   static const Color white = Colors.white;
   static const Color black = Colors.black;
   static const Color textColor = Color(0xFFffffff);
-  static const Color textColorLight = Color(0xFFffffff);
+  static const Color textColorLight = Color(0xFF002147);
   static const Color textColorDark = Color(0xFFffffff);
   static const Color transparent = Colors.transparent;
   static const Color primaryColor = Color(0xff3dd8c5);
@@ -17,6 +17,5 @@ class ManagerColors {
   static const Color containerColor = Color(0xFF515151);
   static const Color red = Color(0xFFFF2400);
   static const Color disabledColor = Colors.grey;
-    static const Color scaffoldBackgroundColorLight = Colors.white;
-
+  static const Color scaffoldBackgroundColorLight = Colors.white;
 }
